ae0570e8da49fd2dd036cdeef14812b53a882948
rahulm682 <maurya.rahul682@gmail.com>
2023-03-16 14:49:09 +0530

    added api for adding attendence, teacher, student

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.post("/login-teacher",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
+                res.send({ success: false, err })
             }
             if (result.length > 0) {
                 res.send({ success: true, msg: "teacher login successful", result })
@@ -70,7 +70,7 @@ app.post("/login-student",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
+                res.send({ success: false, err })
             }
             if (result.length > 0) {
                 res.send({ success: true, msg: "student login successful", result })
@@ -82,36 +82,6 @@ app.post("/login-student",
         })
     })
 
-
-// adding subjects to subject table
-app.post("/add-subject",
-    // body(fieldname, errorMsg)
-    [
-        body("subject_code", "Enter subject code of length 7").isLength({ min: 7, max: 7 }),
-        body("subject_name", "password min length 5").isLength({ min: 4 }),
-    ],
-    async (req, res) => {
-
-        // check for errors in input
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ success: false, errors: errors.array() });
-        }
-
-        const { subject_code, subject_name } = req.body
-
-        const sql = `insert into subject_TB(subject_code, subject_name) values('${subject_code}', '${subject_name}')`
-        connection.query(sql, (err, result) => {
-            if (err) {
-                console.log(err.sqlMessage)
-                res.send(err)
-            }
-
-            if (result) {
-                res.send({ success: true, msg: "subject added successfully" })
-            }
-        })
-    })
 
 
 // all the subjects taken by a teacher
@@ -134,7 +104,7 @@ app.get("/teacher",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
+                res.send({ success: false, err })
             }
 
             if (result) {
@@ -165,8 +135,7 @@ app.get("/student",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
-                return
+                res.send({ success: false, err })
             }
 
             if (result) {
@@ -180,11 +149,11 @@ app.get("/student",
             connection.query(sql, (err, result) => {
                 if (err) {
                     console.log(err.sqlMessage)
-                    res.send(err)
+                    res.send({ success: false, err })
                 }
 
                 if (result) {
-                    res.send(result)
+                    res.send({ success: true, result })
                 } else {
                     res.send({ msg: "no subjects found" })
                 }
@@ -214,12 +183,11 @@ app.get("/students",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
-                return
+                res.send({ success: false, err })
             }
 
             if (result) {
-                res.send(result)
+                res.send({ success: true, result })
             }
         })
     })
@@ -228,12 +196,12 @@ app.get("/students",
 
 
 // api for getting the attendence details of particular student in particular subject
-app.get("/attendence", 
+app.get("/attendence",
     [
         body("student_id", "enter valid student_id").isNumeric(),
         body("subject_code", "Enter subject code of length 7").isLength({ min: 7, max: 7 }),
     ],
-    async(req, res) => {
+    async (req, res) => {
         // check for errors in input
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -246,63 +214,165 @@ app.get("/attendence",
         connection.query(sql, (err, result) => {
             if (err) {
                 console.log(err.sqlMessage)
-                res.send(err)
-                return
+                res.send({ success: false, err })
             }
 
             if (result) {
-                res.send(result)
+                res.send({ success: true, result })
+            }
+        })
+    })
+
+
+app.post("/add-attendence",
+    // validation not done as we are getting the array of students and 
+    // for that purpose we need to check validation for each student
+    // [
+    //     body("student_id", "enter valid student id").isNumeric(),
+    //     body("subject_code", "enter valid subject code").isLength({min:7, max:7}),
+    //     body("teacher_id", "enter valid teacher id").isNumeric(),
+    //     body("date", "enter valid date").isDate(),
+    //     body("present", "enter valid value").isBoolean(),
+    // ],
+    async (req, res) => {
+        // check for errors in input
+        // const errors = validationResult(req);
+        // if (!errors.isEmpty()) {
+        //     return res.status(400).json({ success: false, errors: errors.array() });
+        // }
+
+        const { students } = req.body
+        let sql = `insert into attendance_tb(student_id, subject_code, teacher_id, date, present) values`
+
+        for (let i = 0; i < students.length; i++) {
+            const { student_id, subject_code, teacher_id, date, present } = students[i]
+            sql += `(${student_id}, '${subject_code}', ${teacher_id}, '${date}', ${present})`
+            if (i != students.length - 1) sql += ", "
+        }
+
+        // connection.query(sql, (err, result) => {
+        //     if(err) {
+        //         console.log(err.sqlMessage)
+        //         res.send({success: false, err})
+        //     }
+
+        //     if(result) {
+        //         console.log(result)
+        //         res.send({success: true, msg: "attendence added successfully"})
+        //     }
+
+        // })
+        res.send(sql)
+    })
+
+
+// adding subjects to subject table
+app.post("/add-subject",
+    // body(fieldname, errorMsg)
+    [
+        body("subject_code", "Enter subject code of length 7").isLength({ min: 7, max: 7 }),
+        body("subject_name", "password min length 5").isLength({ min: 4 }),
+    ],
+    async (req, res) => {
+
+        // check for errors in input
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
+
+        const { subject_code, subject_name } = req.body
+
+        const sql = `insert into subject_TB(subject_code, subject_name) values('${subject_code}', '${subject_name}')`
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err.sqlMessage)
+                res.send({ success: false, err })
+            }
+
+            if (result) {
+                res.send({ success: true, msg: "subject added successfully" })
             }
         })
     })
 
 
 
-
-// adding teacher to subject table
-app.post("/add-teacher", async (req, res) => {
-    const { teacher_id, teacher_name, email } = req.body
-
-    const sql = `insert into teacher_TB(teacher_name, email) values('${teacher_name}', '${email}')`
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log(err.sqlMessage)
-            res.send(err)
-        } else {
-            res.send(result)
+// adding teacher
+app.post("/add-teacher",
+    [
+        body("teacher_name", "enter valid name").isLength({ min: 3 }),
+        body("email", "enter valid email").isEmail(),
+        body("password", "enter valid password").isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        // check for errors in input
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
         }
+
+        const { teacher_name, email, password } = req.body
+
+        const sql = `insert into teacher_TB(teacher_name, email_id, password) values('${teacher_name}', '${email}', '${password}')`
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err.sqlMessage)
+                res.send({ success: false, err })
+            }
+
+            if (result) {
+                res.send({ success: true, result })
+            }
+        })
     })
-})
 
 
 // api for sign up of user
-app.post("/student-reg", async (req, res) => {
-    const { name, email, reg_no, course, department, semester } = req.body
-
-    let sql = `select * from student_TB where email=${email}`
-
-    // check if user is already registered or not
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log(err.sqlMessage)
-        } else {
-            console.log("Email already registered")
-            res.send(result)
-            return
+app.post("/add-student",
+    [
+        body("name", "enter valid name").isLength({ min: 3 }),
+        body("email", "enter valid email").isEmail(),
+        body("reg_no", "enter valid Registration number").isLength({ min: 9, max: 9 }),
+        body("course", "enter valid course").isLength({ min: 3 }),
+        body("department", "enter valid department").isLength({ min: 3 }),
+        body("semester", "enter valid semester").isNumeric({ min: 1, max: 6 }),
+        body("password", "enter valid password").isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        // check for errors in input
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
         }
-    })
 
+        const { name, email, reg_no, course, department, semester, password } = req.body
 
-    sql = `insert into student_TB(name, email, reg_no, course, department, semester) values ('${name}', '${email}', '${reg_no}', '${course}', '${department}', '${semester}')`
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log(err.sqlMessage)
-            res.send(err)
-        } else {
-            res.send(result)
-        }
+        // let sql = `select * from student_TB where email=${email}`
+
+        // // check if user is already registered or not
+        // connection.query(sql, (err, result) => {
+        //     if (err) {
+        //         console.log(err.sqlMessage)
+        //     } else {
+        //         console.log("Email already registered")
+        //         res.send(result)
+        //         return
+        //     }
+        // })
+
+        let sql = `insert into student_TB(student_name, email_id, student_regno, course, department, semester, password) values ('${name}', '${email}', '${reg_no}', '${course}', '${department}', '${semester}', '${password}')`
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err.sqlMessage)
+                res.send({ success: false, err })
+            }
+
+            if (result) {
+                res.send({ success: true, result })
+            }
+        })
     })
-})
 
 
 // console.log('todo list RESTful API server started on: ' + port);
