9b049b73d573a72f5ce1de5bd5dcbd103a77b545
rahulm682 <maurya.rahul682@gmail.com>
2023-03-18 11:45:19 +0530

    separated the apis and added the router

---
+++ backend/routes/teacher.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const router = express.Router();
+
+// database connection
+const config = require('../conn')
+const connection = config.connection
+
+// bcryptjs package for password hashing and encryption
+const bcrypt = require('bcryptjs');
+
+// import validators
+const { body, validationResult } = require('express-validator');
+
+// JSON Web Token to generate unique Token for user
+const jwt = require('jsonwebtoken');
+// Signature Key
+const AUTH_KEY = "MYNameISRahul@6820";
+
+// api for login of teacher
+router.post("/login-teacher",
+    // body(fieldname, errorMsg)
+    [
+        body("email", "Enter valid email").isEmail(),
+        body("password", "password min length 5").isLength({ min: 5 })
+    ],
+    async (req, res) => {
+
+        // check for errors in input
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
+
+        const { email, password } = req.body
+
+        let sql = `select teacher_id, email_id, teacher_name from teacher_tb where email_id='${email}' and password='${password}'`
+
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err.sqlMessage)
+                res.send({ success: false, err })
+                return
+            }
+            if (result.length > 0) {
+                res.send({ success: true, msg: "teacher login successful", result })
+                return
+            }
+
+            if (result) {
+                res.send({ msg: "no teacher found" })
+                return
+            }
+        })
+    })
+
+
+
+// all the subjects teached by a teacher
+router.get("/teacher-subjects",
+    // body(fieldname, errorMsg)
+    [
+        body("teacher_id", "teacher ID is number").isNumeric(),
+    ],
+    async (req, res) => {
+
+        // check for errors in input
+        const errors = validationResult(req.query);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ success: false, errors: errors.array() });
+        }
+
+
+        const { teacher_id } = req.query
+
+        const sql = `select subject_name, subject_code from subject_tb where subject_code in(select subject_code from teacher_subjects_tb where teacher_ID=${teacher_id});`
+        connection.query(sql, (err, result) => {
+            if (err) {
+                console.log(err.sqlMessage)
+                res.send({ success: false, err })
+                return
+            }
+
+            if (result.length > 0) {
+                res.send({ success: true, result })
+                return
+            } else if (result.length == 0) {
+                res.send({ success: true, msg: "no subjects found" })
+                return
+            }
+        })
+    })
+    
+
+
+// Export the module
+module.exports = router;
